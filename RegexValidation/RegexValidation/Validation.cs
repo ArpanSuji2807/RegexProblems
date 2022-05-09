9b5b587ab241394ce21b11db8f0833e3d967d05d
Praan26 <arpanap102@gmail.com>
2022-05-09 19:32:50 +0530

    [Arpan] Add: Mobile Number validation is added

--- RegexValidation/RegexValidation/Validation.cs
+++ RegexValidation/RegexValidation/Validation.cs
@@ -5,6 +5,7 @@ namespace RegexValidation
     public class Validation
     {
         const string first_Name = "^[A-Z]{1}[a-z]$";
+        const string mobileNumber = "^[6-9]{1}[0-9]{9}$";
         public bool FirstName_Validation(string name)
         {
             if (Regex.IsMatch(name,first_Name))
@@ -18,6 +19,14 @@ namespace RegexValidation
             if (Regex.IsMatch(last_Name,first_Name))
             {
                 Console.WriteLine("{0} is valid last name", last_Name);
+            }
+            return true;
+        }
+        public bool MobileNumber_Validation(string number)
+        {
+            if (Regex.IsMatch(number,mobileNumber))
+            {
+                Console.WriteLine("{0} is valid number", number);
             }
             return true;
         }
---
+++ RegexValidation/ValidationTests/NumberTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using RegexValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationTests
+{
+    public class NumberTest
+    {
+        [Test]
+        public void FirstNameTest()
+        {
+            Validation validation = new Validation();
+            bool number = validation.MobileNumber_Validation("9456455566");
+            Assert.IsTrue(number);
+        }
+    }
+}
